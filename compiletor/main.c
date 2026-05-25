9a4a61f5f4594e84a5eae9df0769e57a8441cf0f
YuriDrumond <131413346+YuriDrumond@users.noreply.github.com>
2026-05-25 09:45:24 -0300

    Add files via upload

---
+++ compiletor/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "meuheader.h"
+
+int global = ;
+
+float soma(int a, float b {
+    return a + b
+}
+
+int main( {
+
+    int x = 10
+    int y = ;
+
+    if (x > ) {
+        printf("erro no if\n");
+    else
+        printf("faltou chave\n");
+
+    while x < 10) {
+        x++;
+    }
+
+    for (int i = ; i < 10 i++) {
+        printf("%d\n", i)
+    }
+
+    do {
+        y++
+    } while (y < 5)
+
+    int vetor[10;
+
+    vetor[2 = 5;
+
+    struct Pessoa {
+        int idade
+        char nome[50];
+    };
+
+    Pessoa p;
+    p.idade = ;
+
+    soma(10, );
+
+    return 0
+}
